dd580054250776576559501a533063ea74cf3184
chaitali <chaitali.ladikkar@gmail.com>
2018-04-01 09:32:26 -0400

    Player and Loot class added

---
+++ Assets/Scripts/Loot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : IComparable <Loot>
+{
+	public int item_weight;
+	public int item_value;   
+    public float worth;
+	public bool is_visted;
+	public bool is_alarmed;
+	public bool is_secured;
+	public bool is_empty;
+	public bool is_opened;
+	
+    public Loot(int value_item, int weight, bool alarmed, bool secured, bool filled)
+    {
+        item_weight = weight;
+	    item_value = value_item;   
+		worth = item_value / item_weight;
+		is_visted = false;
+		is_alarmed = alarmed;
+		is_secured = secured;
+		is_empty = !filled;
+		is_opened = false;
+    }
+
+    public int CompareTo(Loot l)
+    {
+        if (this.worth < l.worth)
+            return -1;
+        else if (this.worth > l.worth)
+            return 1;
+        else
+            return 0;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+	
+    public static Player instance;
+	private int CAPACITY = 22;
+    private int current_weight;
+	private List<Loot> player_loot; 
+   
+	void Start() 
+	{
+		instance = this;
+		current_weight = 0;
+	}
+
+    public bool pickNewItem(Loot l)
+    {
+       if((current_weight + l.item_weight) <= CAPACITY) { 
+			current_weight += l.item_weight;
+			l.is_visted = true;
+			l.is_empty = true;
+			l.is_opened = true;
+			player_loot.Add(l);
+			return true;
+	   } 
+	   return false;
+    }
+	
+	public bool isFull()
+	{
+		if(current_weight >= CAPACITY) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int percentFull()
+	{
+		return ((current_weight * 100) / CAPACITY);
+	}
+	
+	public bool exchange_item(Loot l) 
+	{
+		if((current_weight + l.item_value) <= CAPACITY) { 
+			current_weight += l.item_weight;
+			l.is_visted = true;
+			l.is_empty = true;
+			l.is_opened = true;
+			player_loot.Add(l);
+			return  true;
+	    } 
+				
+		foreach(Loot lu in player_loot) {
+			if(lu.worth < l.worth) {
+		     	if(lu.item_weight <= l.item_weight) {
+					current_weight = current_weight + l.item_weight - lu.item_weight;
+					lu.is_visted = true;
+					lu.is_empty = true;
+					lu.is_opened = true;
+					lu.item_weight = l.item_weight;
+					lu.item_value = l.item_value;   
+					lu.worth = l.worth;
+					lu.is_secured = l.is_secured;
+					lu.is_alarmed = l.is_alarmed;				
+					return true;
+				} 
+			}
+		}
+		
+		return false;
+	}
+}
+
