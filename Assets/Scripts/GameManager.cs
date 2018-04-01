af31af49feeb09456c72eadae10d61489906feaa
Prasad Madhale <prasadchelsea33@users.noreply.github.com>
2018-04-01 00:40:41 -0400

    Added Timer and updated navmesh to consider the outer plane walkable as well

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour {
 
     public bool AutoMove;
     public GameObject astar;
+    public Text gameover;
 
-    // Update is called once per frame
     void Update () {
 		if(Input.GetKey(KeyCode.Escape))
             SceneManager.LoadScene("Menu");
+
+        if (Timer.instance.timelimit <= 0)
+        {
+            gameover.text = "GAME OVER!";
+            Time.timeScale = 0;
+        }
 
         if (AutoMove)
             astar.SetActive(true);
         else
             astar.SetActive(false);
     }
+
 }
---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public static Timer instance;
+    public Text timer;
+    float startTime;
+    public float timelimit = 60f;
+    
+   
+	// Use this for initialization
+	void Start () {
+        instance = this;
+        startTime = Time.time;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timelimit > 0)
+            timelimit -= Time.deltaTime;
+        else
+            timer.color = Color.red;
+           
+        string timertext = timelimit.ToString("f1");
+        timer.text = "Timer - " + timertext;
+
+    }
+}
