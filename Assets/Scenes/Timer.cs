3cb7aa98acfc1eec74e25a15e5ed1ad1ffcd1624
Varun Sriram <varun.sriram8395@gmail.com>
2018-04-03 16:42:27 -0400

    Different treasure script

---
+++ Assets/Scenes/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public static Timer instance;
+    public Text timer;
+    float startTime;
+    public float timelimit = 60f;
+    public float resetTimer;
+   
+	// Use this for initialization
+	void Start () {
+        instance = this;
+        startTime = Time.time;
+        resetTimer = timelimit;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (timelimit > 0)
+            timelimit -= Time.deltaTime;
+        else
+            timer.color = Color.red;
+           
+        string timertext = timelimit.ToString("f1");
+        timer.text = "Timer - " + timertext;
+
+    }
+}
