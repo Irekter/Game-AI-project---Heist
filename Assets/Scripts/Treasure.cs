4eafe75bd4ba01f8317257e61f8908082098fd48
chaitali <chaitali.ladikkar@gmail.com>
2018-04-03 20:59:36 -0400

    Treasure file modifications

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -3,53 +3,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Treasure : IComparable <Treasure>
+public class Treasure
 {
 	public Vector3 position;
 	public int item_weight;
 	public int item_value;   
-    public float item_worth;
-	public bool is_visted;
-	public bool is_alarmed;
-	public bool is_secured;
-	public bool is_empty;
-	public bool is_opened;
+    public int looting_time;
+	public float item_worth;
+	public bool visted;
+	public bool alarmed;
+	public bool secured;
+	public bool opened;
 	
-    public Treasure(Vector3 _pos, int _value_item, int _weight, bool _alarmed, bool _secured, bool _filled)
+	
+    public Treasure(Vector3 _pos, int _value_item, int _weight, int _looting_time, bool _alarmed, bool _secured)
     {
  		this.position = _pos;
 		this.item_weight = _weight;
 	    this.item_value = _value_item;   
-		if(_value_item != 0) {
+		if(_value_item != 0) 
+		{
 			this.item_worth = _weight / _value_item;
 		} 
-		else {
+		else 
+		{
 			this.item_worth = 0;
 		}
-		this.is_visted = false;
-		this.is_alarmed = _alarmed;
-		this.is_secured = _secured;
-		this.is_empty = !_filled;
-		this.is_opened = false;
+		this.visted = false;
+		this.alarmed = _alarmed;
+		this.secured = _secured;
+		this.opened = false;
+		this.looting_time = _looting_time;
     }
 	
-	void rob_treasure()
+	void empty_treasure()
 	{
 		this.item_value = 0;
 		this.item_weight = 0;
 		this.item_worth = 0;
-		this.is_visted = true;
-		this.is_empty = true;
-		this.is_opened = true;	
+		this.visted = true;
+		this.opened = true;
+		this.looting_time = 0;		
 	}
 	
-    public int CompareTo(Treasure t)
-    {
-        if (this.item_worth < t.item_worth)
-            return -1;
-        else if (this.item_worth > t.item_worth)
-            return 1;
-        else
-            return 0;
+	
+	// Varun's Alarmed Treasure Code
+	/*
+	
+	public GameObject sw;
+    public GameObject time;
+	void Start () {
+        
     }
+	
+	// Update is called once per frame
+	void Update () {
+        //Reference for alarm
+        sw = GameObject.Find("Switch");
+        Button button = sw.GetComponent<Button>();
+
+
+        //Getting referenceof timescript
+        time = GameObject.Find("Timer");
+        Timer timer = time.GetComponent<Timer>();
+        //Check if switch is open 
+        
+        if(button.activated==false)
+        {
+            timer.timelimit -= 10;
+
+        }
+	}
+	
+	*/
 }
